35ae5cf68907705794c69261b486f4b49f8ef0aa
Garron Sanchez <garron.michael@gmail.com>
2015-01-25 22:02:14 -0800

    Match the results of calling getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className) {
   // your code here
+  var found = [];
+  function search(eList, className) {
+  	for(var i = 0; i < eList.length; i++) {
+  		if(eList[i].classList) {
+	  		for(var j = 0; j < eList[i].classList.length; j++) {
+		  		if(eList[i].classList[j] === className) {
+		  			found.push(eList[i]);
+		  		}
+	  		}
+  		} 
+  		if(eList[i].childNodes.length > 0){
+			search(eList[i].childNodes, className);
+  		}
+  	}
+  }
+  search(document.childNodes, className);
+  return found;
 };
